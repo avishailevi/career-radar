793af48150b1ea985b18ddbd6c50f89c921cff56
avishailevi <72466486+avishailevi@users.noreply.github.com>
2026-07-08 19:41:36 +0300

    Route platforms through explicit scanners

--- scanners/scanner_factory.py
+++ scanners/scanner_factory.py
@@ -1,13 +1,25 @@
-from scanner import scan_company
+from scanners.amazon_scanner import AmazonScanner
+from scanners.apple_scanner import AppleScanner
+from scanners.generic_scanner import GenericScanner
+from scanners.google_scanner import GoogleScanner
+from scanners.microsoft_scanner import MicrosoftScanner
 from scanners.workday_scanner import WorkdayScanner
 
 
 class ScannerFactory:
+    SCANNERS = {
+        "amazon": AmazonScanner,
+        "apple": AppleScanner,
+        "custom": GenericScanner,
+        "generic": GenericScanner,
+        "google": GoogleScanner,
+        "microsoft": MicrosoftScanner,
+        "workday": WorkdayScanner,
+    }
+
     @staticmethod
     def scan(company: dict, debug: bool = False) -> list[dict]:
         platform = company.get("platform", "generic")
+        scanner_class = ScannerFactory.SCANNERS.get(platform, GenericScanner)
 
-        if platform == "workday":
-            return WorkdayScanner().scan(company, debug=debug)
-
-        return scan_company(company, debug=debug)
+        return scanner_class().scan(company, debug=debug)
